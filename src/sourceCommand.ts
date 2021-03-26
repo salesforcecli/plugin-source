27f2860f5735e5a40458ff29767b00ad6862bbc5
Steve Hetzel <shetzel@salesforce.com>
2021-03-26 10:11:36 -0600

    refactor: remove commented import

--- src/sourceCommand.ts
+++ src/sourceCommand.ts
@@ -5,7 +5,6 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import * as path from 'path';
-// import { inspect } from 'util';
 import { SfdxCommand } from '@salesforce/command';
 import { ComponentSet } from '@salesforce/source-deploy-retrieve';
 import { fs, SfdxError, Logger } from '@salesforce/core';
