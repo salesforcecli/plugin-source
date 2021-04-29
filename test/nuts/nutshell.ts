d6cb456cdf127e7e896511b8af7606fce25973cb
Willie Ruemmele <willieruemmele@gmail.com>
2021-04-29 07:35:34 -0700

    fix: add NUT testing LWC bug (#70)
    * fix: add NUT testing LWC bug
    
    * fix: update LWC creation step, fix compilation

--- test/nuts/nutshell.ts
+++ test/nuts/nutshell.ts
@@ -9,7 +9,6 @@
 
 import * as path from 'path';
 import * as os from 'os';
-import { copyFile } from 'fs/promises';
 import * as fg from 'fast-glob';
 import { exec } from 'shelljs';
 import { TestSession, execCmd } from '@salesforce/cli-plugins-testkit';
@@ -147,6 +146,15 @@ export class Nutshell extends AsyncCreatable<Nutshell.Options> {
     options: Partial<Nutshell.CommandOpts> = {}
   ): Promise<Result<{ created: string[]; outputDir: string }>> {
     return this.execute('force:apex:class:create', options);
+  }
+
+  /**
+   * Create a Lightning Web Component
+   */
+  public async createLWC(
+    options: Partial<Nutshell.CommandOpts> = {}
+  ): Promise<Result<{ created: string[]; outputDir: string }>> {
+    return this.execute('force:lightning:component:create', options);
   }
 
   /**
@@ -332,10 +340,10 @@ export class Nutshell extends AsyncCreatable<Nutshell.Options> {
       const src = path.join(this.testMetadataFolder, file);
       this.debug(`addTestFiles: ${src} -> ${dest}`);
       try {
-        await copyFile(src, dest);
+        fs.copyFileSync(src, dest);
       } catch {
         await fs.mkdirp(path.dirname(dest));
-        await copyFile(src, dest);
+        fs.copyFileSync(src, dest);
       }
     }
     await this.trackFiles(this.testMetadataFiles);
