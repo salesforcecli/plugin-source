13cd75b47608af55be5e45842bc14edb866c93ec
mshanemc <shane.mclaughlin@salesforce.com>
2022-04-25 19:14:21 -0500

    Revert "test: nuts for nested LWC"
    This reverts commit 43228d112fb051154d20107e0dba297f3304fb8c.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,5 @@
  */
 module.exports = {
   extends: ['eslint-config-salesforce-typescript', 'eslint-config-salesforce-license'],
-  ignorePatterns: ['test/nuts/ebikes-lwc/**', 'test/nuts/nestedLWCProject/**'],
+  ignorePatterns: ['test/nuts/ebikes-lwc/**'],
 };
--- test/nuts/nestedLWCProject/force-app/main/default/lwc/cmpA/cmpA.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from "lwc";
-
-export default class CmpA extends LightningElement {}
